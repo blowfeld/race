0b43a2f6568a3bc5c512d98c421194fd9e4f6fd8
Thomas Baier <tbaier@math.bme.hu>
2014-04-29 02:28:10 +0200

    Added javadoc

--- clocking/src/main/java/thomasb/web/clocking/ClockedRequestProcessor.java
+++ clocking/src/main/java/thomasb/web/clocking/ClockedRequestProcessor.java
@@ -4,6 +4,21 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 public interface ClockedRequestProcessor {
-	void service(ServletRequest request, ServletResponse response);
-	void timeoutResponse(int time, ServletResponse response);
+
+	/**
+	 * Processes the request for the given time interval count.
+	 * 
+	 * @param requestTime the time interval count the request belongs to
+	 * @param request the client request to be processed
+	 * @param response the response object to be written
+	 */
+	void service(int requestTime, ServletRequest request, ServletResponse response);
+	
+	/**
+	 * Processes the response in case it is not received in time.
+	 * 
+	 * @param requestTime the time interval count the request was intended for
+	 * @param response the response object to be written to
+	 */
+	void timeoutResponse(int requestTime, ServletResponse response);
 }
