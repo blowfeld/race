f1a598376a3c441df1f52edca5b3199485b6f5ab
Thomas Baier <tbaier@math.bme.hu>
2014-04-29 02:40:49 +0200

    Use http specific methods, added javadoc

--- clocking/src/main/java/thomasb/web/clocking/ClockedServlet.java
+++ clocking/src/main/java/thomasb/web/clocking/ClockedServlet.java
@@ -4,10 +4,21 @@ import java.io.IOException;
 
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+/**
+ * A {@code ClockedServlet} synchronizes between request of its participants.
+ * 
+ * The participants are supposed to send requests in fixed time intervals. For
+ * each time interval the responses to the participants are delayed until the
+ * request of the last participant arrives or a timeout limit is reached.
+ * 
+ * Participants must specify the count of the intended time interval in their
+ * request and must not send a request to the succeeding interval before they
+ * received the response from the server or a given timeout is reached.
+ */
 @SuppressWarnings("serial")
 public class ClockedServlet extends HttpServlet {
 	private final ClockedSubmissionThread submissionThread;
@@ -22,16 +33,22 @@ public class ClockedServlet extends HttpServlet {
 	}
 	
 	@Override
-	public void service(ServletRequest request,
-			ServletResponse response) throws ServletException, IOException {
-		int requestTime = (int)request.getAttribute("time");
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		int requestTime = readTime(request);
 		if (submissionThread.getIntervalCount() > requestTime) {
+			requestProcessor.timeoutResponse(requestTime, response);
 			return;
 		}
 		
-		requestProcessor.service(request, response);
+		requestProcessor.service(requestTime, request, response);
 
 		AsyncContext async = request.startAsync(request, response);
 		submissionThread.addRequest(async, requestTime);
+	}
+
+	private int readTime(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
