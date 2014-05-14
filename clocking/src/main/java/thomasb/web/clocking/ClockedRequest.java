4f4a6a6b9b52e37c6f646fdc18a8586c9a924670
Thomas Baier <tbaier@math.bme.hu>
2014-05-14 18:42:30 +0200

    Made ClockedRequest public again

--- clocking/src/main/java/thomasb/web/clocking/ClockedRequest.java
+++ clocking/src/main/java/thomasb/web/clocking/ClockedRequest.java
@@ -2,26 +2,26 @@ package thomasb.web.clocking;
 
 import javax.servlet.AsyncContext;
 
-final class ClockedRequest<T> {
+public final class ClockedRequest<T> {
 	private final AsyncContext context;
 	private final T data;
 	private final int requestTime;
 	
-	ClockedRequest(AsyncContext context, T data, int requestTime) {
+	public ClockedRequest(AsyncContext context, T data, int requestTime) {
 		this.context = context;
 		this.data = data;
 		this.requestTime = requestTime;
 	}
 	
-	AsyncContext getContext() {
+	public AsyncContext getContext() {
 		return context;
 	}
 	
-	T getData() {
+	public T getData() {
 		return data;
 	}
 	
-	int getTime() {
+	public int getTime() {
 		return requestTime;
 	}
 }
